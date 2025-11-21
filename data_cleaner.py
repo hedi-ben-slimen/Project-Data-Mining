4bd34e9168ad7439a561c69d30815651fe4d2d41
Assad Ben Brahim <assad.ben.brahiim@gmail.com>
2025-11-21 16:53:53 +0100

    Add Cleaning Code

--- data_cleaner.py
+++ data_cleaner.py
@@ -24,20 +24,18 @@ df_clean = df.dropna(subset=['name'])
 print(f"After removing missing names: {df_clean.shape}")
 
 # Remove duplicates
-df_clean = df_clean.drop_duplicates(subset=['name', 'price'])
+df_clean = df_clean.drop_duplicates(subset=['url'])
 print(f"After removing duplicates: {df_clean.shape}")
 
 # Turning prices to floats
-df_clean['price'] = df_clean['price'].str.replace('Dhs', '', regex=False)
-df_clean['price'] = df_clean['price'].str.replace(",", "")
+df_clean['price'] = df_clean['price'].str.replace('Dhs', '', regex=False).str.replace(",", "", regex=False)
 
 df_clean['price'] = df_clean['price'].apply(
     lambda x: (float(x.split(' - ')[0]) + float(x.split(' - ')[1])) / 2 if ' - ' in x else float(x)
 )
 
+
 df_clean['price'] = df_clean['price'].astype(float)
-
-
 
 # Create new features
 # Uncomment after adding discounts to scrapped data
@@ -45,9 +43,12 @@ df_clean['price'] = df_clean['price'].astype(float)
 
 
 # To be modified when discounts added
-#df_clean['Price_Category'] = pd.cut(df_clean['price'], 
-                                      #bins=[0, 50, 200, 650, 1000], 
-                                      #labels=['Low', 'Medium', 'High', 'Premium'])
+price_bins = df_clean['price'].quantile([0, .25, .5, .75, 1]) # Categorizing data ensuring 25% of the data falls into each category
+df_clean['Price_Category'] = pd.cut(df_clean['price'], 
+                                      bins = price_bins, 
+                                      labels = ['Low', 'Medium', 'High', 'Premium'],
+                                      include_lowest = True
+                                    )
 
 # Handle missing old prices
 # Uncomment after adding discounts to scrapped data
@@ -55,8 +56,6 @@ df_clean['price'] = df_clean['price'].astype(float)
 
 print("\nCleaned data summary:")
 print(df_clean.describe())
-
-products_with_range = df_clean[df_clean['price'].str.contains(' - ')]
-
-# Display the relevant columns to inspect the products
-print(products_with_range[['name', 'price']])
+print(df_clean.head())
+print(df_clean.max())
+print(df_clean.max()['url'])
